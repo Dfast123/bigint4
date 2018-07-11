70a4e8322a9669de0cd6aae836cab60ec7db466e
Dfast123 <41119071+Dfast123@users.noreply.github.com>
2018-07-11 17:08:41 +0300

    Initial commit

---
+++ BigInt.cs
@@ -0,0 +1,395 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigInt4
+{
+    class BigInt
+    {
+        private bool isNegative;
+
+        public bool IsNegative
+        {
+            get { return isNegative; }
+            set { isNegative = value; }
+        }
+
+        private string number;
+
+        public string Number
+        {
+            get { return number; }
+            set { number = value; }
+        }
+
+        public BigInt(string number, bool isNegative)
+        {
+            this.Number = number;
+            this.IsNegative = isNegative;
+        }
+        
+
+        public static BigInt Sum(BigInt number1, BigInt number2)
+        {
+
+            bool ResIsNegative = number1.IsNegative;
+
+            if (number1.isNegative && !number2.IsNegative)//-+
+            {
+                return Substract(number2, new BigInt(number1.Number, !number1.IsNegative));
+            }
+
+            if (!number1.isNegative && number2.IsNegative)//+-
+            {
+                return Substract(number1, new BigInt(number2.number, !number2.IsNegative));
+            }
+
+
+
+
+            int length = number1.number.Length;
+            if(number1.number.Length < number2.number.Length)
+            {
+                length = number2.number.Length;
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            string ResNumber;
+            int curr = 0;
+            int NaUm = 0;
+            int curr1 = 0;
+            int curr2 = 0;
+
+
+
+            StringBuilder sbNum1 = new StringBuilder();
+            for (int i = 0; i < length; i++)
+            {
+                try
+                {
+                    sbNum1.Append(number1.number[number1.number.Length - i - 1]);
+                }
+                catch
+                {
+                    sbNum1.Append("0");
+                }
+            }
+
+            StringBuilder sbNum2 = new StringBuilder();
+            for (int i = 0; i < length; i++)
+            {
+                try
+                {
+                    sbNum2.Append(number2.number[number2.number.Length - i - 1]);
+                }
+                catch
+                {
+                    sbNum2.Append("0");
+                }
+            }
+
+
+
+            for (int i = 0; i < length; i++)
+            {
+                try
+                {
+                    curr1 = int.Parse(sbNum1[i].ToString());
+                }
+                catch
+                {
+                    curr1 = 0;
+                }
+                try
+                {
+                    curr2 = int.Parse(sbNum2[i].ToString());
+                }
+                catch
+                {
+                    curr2 = 0;
+                }
+                
+                curr = (NaUm + curr1 + curr2);
+                NaUm = curr / 10;
+                curr %= 10;
+                sb.Append(curr.ToString());
+            }
+            if (NaUm != 0)
+                sb.Append(NaUm);
+
+
+            StringBuilder sbReverse = new StringBuilder();
+            for (int i = sb.Length-1; i >= 0; i--)
+            {
+                sbReverse.Append(sb[i]);
+            }
+            ResNumber = sbReverse.ToString();
+
+            return new BigInt(ResNumber, ResIsNegative);
+
+            
+        }
+        
+        public static BigInt Substract(BigInt number1, BigInt number2)
+        {
+
+            bool ResIsNegative = number1.IsNegative;            
+            if(number1.isNegative && !number2.IsNegative)//-+
+            {
+                return Sum(number1, new BigInt(number2.number, !number2.IsNegative));
+            }
+
+            if(!number1.isNegative && number2.IsNegative)//+-
+            {
+                return Sum(number1, new BigInt(number2.number, !number2.IsNegative));
+            }
+            
+            
+
+            int length = number1.number.Length;
+            if (number1.number.Length < number2.number.Length)
+            {
+                length = number2.number.Length;
+            }
+
+                 StringBuilder sbNum1 = new StringBuilder();
+            for (int i = 0; i < length; i++)
+            {
+                try
+                {
+                    sbNum1.Append(number1.number[number1.number.Length - i - 1]);
+                }
+                catch
+                {
+                    sbNum1.Append("0");
+                }
+            }
+
+            StringBuilder sbNum2 = new StringBuilder();
+            for (int i = 0; i < length; i++)
+            {
+                try
+                {
+                    sbNum2.Append(number2.number[number2.number.Length - i - 1]);
+                }
+                catch
+                {
+                    sbNum2.Append("0");
+                }
+            }
+
+
+            if (ReturnBigger(number1, number2) == number2)
+            {
+                StringBuilder sbSwap = sbNum1;
+                sbNum1 = sbNum2;
+                sbNum2 = sbSwap;
+                //ako 2roto e po golqmo, se obrushta znaka!!!
+                ResIsNegative = !ResIsNegative;
+            }
+
+            string ResNumber;
+            int curr1;
+            int curr2;
+            int curr;
+            int NaUm = 0;
+
+
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < length; i++)
+            {
+                try
+                {
+                    curr1 = int.Parse(sbNum1[i].ToString());
+                }
+                catch
+                {
+                    curr1 = 0;
+                }
+                try
+                {
+                    curr2 = int.Parse(sbNum2[i].ToString());
+                }
+                catch
+                {
+                    curr2 = 0;
+                }
+
+                curr = (curr1 - curr2 - NaUm);
+                if (curr < 0)
+                {
+                    curr += 10;
+                    NaUm = 1;
+                }
+                sb.Append(curr.ToString());
+            }
+
+
+            StringBuilder sbReverse = new StringBuilder();
+            for (int i = sb.Length - 1; i >= 0; i--)
+            {
+                sbReverse.Append(sb[i]);
+            }
+            ResNumber = sbReverse.ToString();
+
+            return new BigInt(ResNumber, ResIsNegative);
+            
+
+
+
+        }
+
+        public static BigInt ReturnBigger(BigInt number1, BigInt number2)
+        {           
+            if (number1.number.Length < number2.number.Length)
+            {
+                return number2;
+            }
+            else if(number1.number.Length > number2.number.Length)
+            {
+                return number1;
+            }
+            else
+            {
+                for(int i=0;i<number1.number.Length;i++)
+                {
+                    if(number1.number[i]>number2.number[i])
+                    {
+                        return number1;
+                    }
+                    else if(number1.number[i]<number2.number[i])
+                    {
+                        return number2;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+
+                }
+
+
+            }
+            return number1;
+
+
+
+
+        }
+
+        public static BigInt Multiply(BigInt number1, BigInt number2)
+        {
+            BigInt result = new BigInt("0", false);
+     
+            int length = number1.number.Length;
+            if (number1.number.Length < number2.number.Length)
+            {
+                length = number2.number.Length;
+            }
+
+
+
+
+
+
+            StringBuilder sbNum1 = new StringBuilder();
+            for (int i = 0; i < length-number1.Number.Length; i++)
+            {
+                sbNum1.Append("0");
+            }          
+            sbNum1.Append(number1.number);
+    
+            
+
+            StringBuilder sbNum2 = new StringBuilder();
+            for (int i = 0; i < length - number2.Number.Length; i++)
+            {
+                sbNum2.Append("0");
+            }
+            sbNum2.Append(number2.number);
+              
+
+            for (int i = 0; i < length;i++ )
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Clear();
+
+               
+
+                int a = int.Parse(sbNum1[length - 1 - i].ToString());
+                sb.Append(sbNum2.ToString());
+
+
+
+                for (int j = 0; j < i; j++)
+                    sb.Append("0");               
+
+                for(int k=0;k<a;k++)
+                {
+                    result = Sum(result, new BigInt(sb.ToString(), false));
+
+                }
+            }
+
+            if (number1.IsNegative != number2.IsNegative)
+            {
+                result.IsNegative = true;
+            }
+
+                return result;
+        }
+
+     /*   public static BigInt Divide(BigInt number1, BigInt number2)
+        {
+            BigInt number2pos = new BigInt(number2.Number,false); 
+            BigInt result = new BigInt("0", false);//otgovora, samo se dolepq sbRes do nego
+            int length = number1.number.Length;
+            if (number1.number.Length < number2.number.Length)
+            {
+                length = number2.number.Length;
+            }
+
+            StringBuilder sb = new StringBuilder();//oborotnoto, s nego se izvurshvat deistviqta
+            StringBuilder sbRes = new StringBuilder();
+
+            for (int i = 0; i < length; i++)
+            {
+                int a = 0;
+                Console.WriteLine("a="+a);
+                
+                sb.Append(number1.Number[i].ToString());
+                BigInt curr = new BigInt(sb.ToString(), false);
+                while(ReturnBigger(curr,number2pos)==curr)
+                {
+                    Console.WriteLine(curr.Number);
+                    a++;
+                    curr = Substract(curr, number2pos);
+                    Console.ReadKey();
+                }
+                sb.Clear();
+                sb.Append(curr.Number);
+
+                sbRes.Append(a.ToString());
+
+
+            }
+
+
+
+
+                result.Number = sbRes.ToString();
+                if (number1.IsNegative != number2.IsNegative)
+                {
+                    result.IsNegative = true;
+                }
+
+            
+            return result;
+
+        }*/
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigInt4
+{
+    class Program
+    {
+        static BigInt Input(string input)
+        {
+            input=input.TrimStart('0');
+            BigInt number = new BigInt(input, false);
+            try
+            {
+
+
+                if (input[0] == '-')
+                {
+                    input = input.TrimStart('-');
+                    input = input.TrimStart('0');
+                    number = new BigInt(input, true);
+
+                }
+                return number;
+            }
+            catch
+            {
+                return number;
+            }
+        }
+
+        static void Describe(BigInt number)
+        {
+            if (number.IsNegative)
+                Console.Write("-");
+
+            Console.WriteLine(number.Number);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("vuvedi chislo 1");
+            string input1 = Console.ReadLine();
+            BigInt number1 = Input(input1);
+            
+
+            Console.WriteLine("vuvedi chislo 2");
+            string input2 = Console.ReadLine();
+            BigInt number2 = Input(input2);
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Describe(number1);
+            Describe(number2);
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            Console.WriteLine("result Plus :");
+            BigInt result = BigInt.Sum(number1, number2);
+            Describe(result);
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("result Minus :");
+            BigInt result2 = BigInt.Substract(number1, number2);
+            Describe(result2);
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("result Multiplication :");
+            BigInt result3 = BigInt.Multiply(number1, number2);
+            Describe(result3);
+            Console.WriteLine();
+            Console.WriteLine();
+
+          /*  Console.WriteLine("result Division :");
+            BigInt result5 = BigInt.Divide(number1, number2);
+            Describe(result5);
+            Console.WriteLine();
+            Console.WriteLine();
+            */
+
+
+            Console.WriteLine("sum of result + and result -");
+            BigInt result4 = BigInt.Sum(result, result2);
+            Describe(result4);
+
+
+
+        }
+    }
+}
